516e9da684ba8009dd6e44ff11e842c51c4a4328
ajaymaddela <157569991+ajaymaddela@users.noreply.github.com>
2024-05-28 16:55:45 +0530

    Update providers.tf

--- deployment/terraform/providers.tf
+++ deployment/terraform/providers.tf
@@ -1,17 +1,20 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      source = "hashicorp/azurerm"
+      version = "3.105.0"
     }
   }
+}
+
+
   #backend "azurerm" {
    # resource_group_name  = "sample"
  #   storage_account_name = "ltbackendstorage"
   #  container_name       = "tfstate"
   #  key                  = "springpetclinic.tfstate"
   #}
-}
+
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {

59a15c956f975d3449c6617c1cae66dbe21260c0
ajaymaddela <157569991+ajaymaddela@users.noreply.github.com>
2024-11-18 15:28:12 +0530

    Update main.tf

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_kubernetes_cluster" "example" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_DC2s_v2"
   }
 
   identity {
